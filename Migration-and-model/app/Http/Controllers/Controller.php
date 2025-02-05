7ead059a28fdea814693003977b97d7b4911b496
kyla <116809759+Kylabear@users.noreply.github.com>
2025-02-05 06:09:23 +0000

    debug

--- Migration-and-model/app/Http/Controllers/Controller.php
+++ Migration-and-model/app/Http/Controllers/Controller.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
 
-class GreetController extends Controller
+class Controller extends BaseController
 {
-    public function greet($name)
-    {
-        return view('greet', ['name' => $name]);
-    }
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
